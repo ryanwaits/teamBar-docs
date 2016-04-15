b1091f0a487f43a2ab64d81a41c54481045cbbf8
Ryan Waits <ryan.waits@gmail.com>
2016-04-15 16:22:41 -0500

    update sample response content

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var markdown = require('markdown').markdown;
+var sanitizeHtml = require('sanitize-html');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index', {
+    markdown: markdown,
+    sanitizeHtml: sanitizeHtml
+  });
 });
 
 module.exports = router;
